2b313cc4d2546c2127e89838bfeca255cd374aac
Christopher Porter <snooper90@gmail.com>
2016-02-26 22:14:50 -0600

    Created the first draft of the getWinner() function

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -31,10 +31,45 @@ function getComputerMove(move) {
 
 function getWinner(playerMove,computerMove) {
     var winner;
-    // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
-    // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
-    // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    /* YOUR CODE HERE */
+    if (playerMove === 'rock'){
+        switch(computerMove){
+            case('rock'):
+                winner = 'tie'
+                break;
+            case('paper'):
+                winner = 'computer';
+                break;
+            default:
+                winner = 'player';
+
+        }
+
+    } else if (playerMove === "paper"){
+        switch(computerMove){
+            case('paper'):
+                winner = 'player'
+                break;
+            case('paper'):
+                winner = 'tie';
+                break;
+            default:
+                winner = 'computer';
+
+        }
+
+    }else{
+        switch(computerMove){
+            case('rock'):
+                winner = 'computer'
+                break;
+            case('paper'):
+                winner = 'player';
+                break;
+            default:
+                winner = 'tie';
+
+        }
+    }
     return winner;
 }
 
